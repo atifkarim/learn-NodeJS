d292da7568633c79e8708a1e6f3c33cb5ba485ba
atifkarim <atif0902082@gmail.com>
2023-12-27 09:48:48 +0100

    Approach to see how server event on

--- l_5_core_module/http.js
+++ l_5_core_module/http.js
@@ -3,5 +3,9 @@ const http = require('http');
 // This server actually is a event emitter
 const server = http.createServer();
 
+server.on('connection', () => {
+    console.log('New connection');
+});
+
 server.listen(3000);
 console.log('Server is listening');
