6988ef104319967a0f24a31d6bd5c9d028863047
atifkarim <atif0902082@gmail.com>
2023-12-27 09:52:30 +0100

    Add req, response

--- l_5_core_module/http.js
+++ l_5_core_module/http.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 // This server actually is a event emitter
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    res.write('Hello guys');
+    res.write('Alles Gut!');
+    res.end();
+});
 
 server.on('connection', () => {
     console.log('New connection');
