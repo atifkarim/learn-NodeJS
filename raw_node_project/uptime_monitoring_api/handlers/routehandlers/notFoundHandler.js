c42add71b08a4a1ab3876840bbca1d0be60cdb58
atifkarim <atif0902082@gmail.com>
2024-01-01 16:24:03 +0100

    A handler which will help if user pass any unlisted route

---
+++ raw_node_project/uptime_monitoring_api/handlers/routehandlers/notFoundHandler.js
@@ -0,0 +1,16 @@
+/*
+ * @File Name: notFoundHandler.js
+ * @Title: Not Found Handler
+ * @Description: Not Found Handler
+ * @Author: Atif Karim
+ * @Date: 01/01/2024
+ */
+
+// module scaffolding
+const handler = {};
+
+handler.notFoundHandler = (req, res) => {
+    console.log('Not Found Handler');
+};
+
+module.exports = handler;
