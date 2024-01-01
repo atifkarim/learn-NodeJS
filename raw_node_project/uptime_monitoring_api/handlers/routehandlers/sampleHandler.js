4ec2982a0dc389b6b22ba9e14526c3c74fbe1788
atifkarim <atif0902082@gmail.com>
2024-01-01 16:23:14 +0100

    A sample handler of routing is written

---
+++ raw_node_project/uptime_monitoring_api/handlers/routehandlers/sampleHandler.js
@@ -0,0 +1,16 @@
+/*
+ * @File Name: sampleHandler.js
+ * @Title: Sample Handler
+ * @Description: Sample Handler
+ * @Author: Atif Karim
+ * @Date: 01/01/2024
+ */
+
+// module scaffolding
+const handler = {};
+
+handler.sampleHandler = (req, res) => {
+    console.log('Sample Handler');
+};
+
+module.exports = handler;
