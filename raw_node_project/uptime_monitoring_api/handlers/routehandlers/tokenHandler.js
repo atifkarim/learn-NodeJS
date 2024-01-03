70aca486bbafc95236d2c48399cc2fbe54ae7e6c
atifkarim <atif0902082@gmail.com>
2024-01-03 10:42:59 +0100

    Setup GET method for token authentication
    - In POSTMAN select GET method and write in address bar: http://localhost:5000/token?id=A_VALID_TOKENID_FROM_TOKEN_FILES

--- raw_node_project/uptime_monitoring_api/handlers/routehandlers/tokenHandler.js
+++ raw_node_project/uptime_monitoring_api/handlers/routehandlers/tokenHandler.js
@@ -73,7 +73,31 @@ handler._token.post = (requestProperties, callback) => {
     }
 };
 
-handler._token.get = (requestProperties, callback) => {};
+handler._token.get = (requestProperties, callback) => {
+    // check the id if valid
+    const id =
+        typeof requestProperties.queryStringObject.id === 'string' &&
+        requestProperties.queryStringObject.id.trim().length === 20
+            ? requestProperties.queryStringObject.id
+            : false;
+    if (id) {
+        // lookup the token
+        data.read('tokens', id, (err, tokenData) => {
+            const token = { ...parseJSON(tokenData) };
+            if (!err && token) {
+                callback(200, token);
+            } else {
+                callback(404, {
+                    error: 'Requested token was not found!',
+                });
+            }
+        });
+    } else {
+        callback(404, {
+            error: 'Requested token was not found!',
+        });
+    }
+};
 
 handler._token.put = (requestProperties, callback) => {};
 
