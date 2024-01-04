61b6ab229484265a12868d4b1682793beecc69f1
atifkarim <atif0902082@gmail.com>
2024-01-04 08:39:52 +0100

    Prepare the boiler plate of notifications
    Here, Twilio will be used to send message

---
+++ raw_node_project/uptime_monitoring_api/helpers/notifications.js
@@ -0,0 +1,18 @@
+/*
+ * Title: Notifications Library
+ * Description: Important functions to notify users
+ * Author: Atif Karim
+ * Date: 04/01/2024
+ *
+ */
+
+// dependencies
+const https = require('https');
+// module scaffolding
+const notifications = {};
+
+// send sms to user using twilio api
+notifications.sendTwilioSms = (phone, msg, callback) => {};
+
+// export the module
+module.exports = notifications;
