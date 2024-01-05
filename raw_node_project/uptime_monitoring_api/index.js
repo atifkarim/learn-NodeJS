d0e61648083c64b6b991932920e72317f8b0c8a7
atifkarim <atif0902082@gmail.com>
2024-01-05 08:23:37 +0100

    Include worker and server library

--- raw_node_project/uptime_monitoring_api/index.js
+++ raw_node_project/uptime_monitoring_api/index.js
@@ -1,66 +1,29 @@
 /*
  * @File Name: index.js
- * @Title: Uptime monitoring application
- * @Description: A RESTFul API to monitor up or down time of user defined links
+ * @Title: Initial file of the project
+ * @Description: This will start the server and workers
  * @Author: Atif Karim
  * @Date: 01/01/2024
  */
 
 // dependencies
 
-const http = require('http');
-require('dotenv').config();
-const { handleReqRes } = require('./helpers/handleReqRes.js');
-const environment = require('./helpers/environments.js');
-const data = require('./lib/data');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 const { sendTwilioSms } = require('./helpers/notifications');
 
 // app object - module scaffolding
-
 const app = {};
 
-// Get user phone number
-// const { myPhone } = process.env;
-
-// @TODO remove later
-// sendTwilioSms(`${myPhone}`, 'Hello world', (err) => {
-//     console.log('Twilio Error this is the error', err);
-// });
-
-// // testing purpose to create file
-// data.create('test', 'newFile', { land: 'Bangladesh', language: 'bangla' }, (createErr) => {
-//     console.log('Creating Error : ', createErr);
-// });
-
-// // testing purpose to read file
-// data.read('test', 'newFile', (readErr, readData) => {
-//     console.log('Read Error: ', readErr, ' -- ', 'Read Data: ', readData);
-// });
-
-// // testing purpose to update file
-
-// data.update('test', 'newFile', { land: 'Germany', language: 'Deutsch' }, (updateErr) => {
-//     console.log('Update Error: ', updateErr);
-// });
-
-// // testing purpose to update file
-// data.delete('test', 'newFile', (deleteErr) => {
-//     console.log('Delete Error: ', deleteErr);
-// });
-
-// create server
-
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        console.log(`Server is listening to ${environment.port} port`);
-    });
+app.init = () => {
+    // start the server
+    server.init();
+    // start the workers
+    workers.init();
 };
 
-// handle request response
+app.init();
 
-app.handleReqRes = handleReqRes;
-
-// start the server
-app.createServer();
+// export the app
+module.exports = app;
