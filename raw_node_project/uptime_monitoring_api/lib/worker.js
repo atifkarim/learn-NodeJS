abf209c6dfed4b43d1e12a57d662331b524a4b87
atifkarim <atif0902082@gmail.com>
2024-01-05 08:23:23 +0100

    Prepare boiler plate of worker library

---
+++ raw_node_project/uptime_monitoring_api/lib/worker.js
@@ -0,0 +1,21 @@
+/*
+ * @File Name: worker.js
+ * @Title: Worker related file
+ * @Description: This will start the worker
+ * @Author: Atif Karim
+ * @Date: 05/01/2024
+ */
+
+// dependencies
+
+// worker object - module scaffolding
+
+const worker = {};
+
+// start the worker
+worker.init = () => {
+    console.log('worker started');
+};
+
+// export
+module.exports = worker;
